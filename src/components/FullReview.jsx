98428a971294d5474000b2b4b660cd08a47546b5
fishertom <tfisher92@gmail.com>
2023-02-06 15:27:38 +0000

    added img alt

--- src/components/FullReview.jsx
+++ src/components/FullReview.jsx
@@ -21,7 +21,7 @@ function Review() {
           <h2 className='full-review-title'>{review.title}</h2>
           <h3>Category: {review.category}</h3>
           <h3>Game designer: {review.designer}</h3>
-          <img className='full-review-picture' src={review.review_img_url} alt="" />
+          <img className='full-review-picture' src={review.review_img_url} alt={"picture for review "+ review.title } />
           <p className='full-review-body'>{review.review_body}</p>
           <p>Author: {review.owner}</p>
           <h3>Votes: {review.votes}</h3>
