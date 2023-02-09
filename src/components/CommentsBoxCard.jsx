2f6427a1d2fa548214d51dc05d249a6800eb5e51
fishertom <tfisher92@gmail.com>
2023-02-09 16:57:04 +0000

    implament delete comment by logged in user, network error feedback

--- src/components/CommentsBoxCard.jsx
+++ src/components/CommentsBoxCard.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { getUserbyUsername } from '../utils/api'
+import { useContext } from 'react';
+import { UserContext } from '../contexts/UserProvider'
+import { deleteComment } from '../utils/api';
 
-function CommentsBoxCard({comment}) {
 
-    const [authorImg, setAuthorImg] = useState('')
+
+function CommentsBoxCard({comment, setComments}) {
+  
+  const { loggedInUser} = useContext(UserContext);
+  const [authorImg, setAuthorImg] = useState('')
+  const [deleteErr, setDeleteErr] = useState(false)
 
    useEffect(() => {
     getUserbyUsername(comment.author).then((user) => {
@@ -11,10 +18,29 @@ function CommentsBoxCard({comment}) {
     })
    }, [comment.author])
 
+   function handleDeleteComment(e) {
+
+    setDeleteErr(false)
+    deleteComment(comment.comment_id)
+    .then(() => {
+      setComments((currentComments) => {
+        return currentComments.filter((filterComment) => filterComment != comment )
+      })
+    })
+    .catch(() => {
+       setDeleteErr(true)
+    })
+   
+   }
+
   return (
     <li className='comment-card'>
+        <p className='delete-error-msg'>{deleteErr? `Network Error`:``}</p>
         <img className='comment-card-img' src={authorImg} alt="" />
         <p className='comment-card-body'>{comment.body}</p>
+        {comment.author === loggedInUser.username ? 
+        <button className='comment-delete-button' onClick={handleDeleteComment}>❌</button>
+      :<></>}
     </li>
   )
 }
